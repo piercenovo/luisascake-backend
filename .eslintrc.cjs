7fbdd36ed423d6a651543a6133d1566adc0ab6ac
piercenovo <piercenovoah@gmail.com>
2022-10-12 14:39:20 -0500

    init: initial project

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    camelcase: 'off',
+    'prefer-promise-reject-errors': 'off'
+  }
+}
